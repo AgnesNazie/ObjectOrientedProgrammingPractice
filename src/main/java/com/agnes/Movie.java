e79aca329bf4901c7a548a4908dc3986ba6325a7
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-24 16:25:12 +0100

    more OOPs class, objects and methods

---
+++ src/main/java/com/agnes/Movie.java
@@ -0,0 +1,10 @@
+package com.agnes;
+
+public class Movie {
+    String name;
+    int ratings;
+
+    void showRatings() {
+        System.out.println(name + " has a rating of " + ratings + "/10.");
+    }
+}
---
+++ src/main/java/com/agnes/MovieRatings.java
@@ -0,0 +1,10 @@
+package com.agnes;
+
+public class MovieRatings {
+    public static void main(String[] args) {
+        Movie m1 = new Movie();
+        m1.name  = "life and love";
+        m1.ratings = 9;
+        m1.showRatings();
+    }
+}
