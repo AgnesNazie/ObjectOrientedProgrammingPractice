6159f4dc38acf792dfa1c13b9c41bea5ed8cb77c
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-24 16:40:43 +0100

    more OOPs class, objects and methods to perform simple operations

---
+++ src/main/java/com/agnes/Calculator.java
@@ -0,0 +1,13 @@
+package com.agnes;
+
+public class Calculator {
+        int a;
+        int b;
+
+         void add() {
+            System.out.println(a + b);
+        }
+         void multiply() {
+            System.out.println(a * b);
+        }
+    }
---
+++ src/main/java/com/agnes/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package com.agnes;
+
+public class SimpleCalculator {
+    public static void main (String []args) {
+        Calculator operations = new Calculator();
+        operations.a = 20;
+        operations.b = 30;
+        operations.add();
+        operations.multiply();
+
+
+    }
+}
