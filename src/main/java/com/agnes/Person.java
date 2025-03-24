625313b405bfd9b043d7eed9a28e98b1a6ab6450
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-24 21:03:02 +0100

    practice method overriding in inheritance

--- src/main/java/com/agnes/Person.java
+++ src/main/java/com/agnes/Person.java
@@ -19,25 +19,41 @@ public class Person {
 }
 
 class Student1 extends Person {
-    String subject;
+    String studentId;
 
     // create constructor for student class
-    public Student1(String name, int age, String subject) {
+    public Student1(String name, int age, String studentId) {
         super(name, age);
-        this.subject = subject;
+        this.studentId = studentId;
     }
 
     @Override
     public void introduce() {
-        System.out.println("Hello, ny name is " + name + " and i am " + age + " years old." + " I am studying " + subject);
+        System.out.println("Hello, ny name is " + name + " and i am " + age + " years old." + " my Student ID is " + studentId);
+    }
+}
+class Teacher extends Person {
+    String subject;
+
+    //create constructor for teacher class
+    public Teacher(String name, int age, String subject) {
+        super(name,age);
+        this.subject = subject;
+    }
+    @Override
+    public void introduce() {
+        System.out.println("Hello, ny name is " + name + " and i am " + age + " years old." + " I am teaching " + subject);
     }
 
 public  static void main (String[] args) {
         Person person = new Person("Nazie", 29);
         person.introduce();
 
-        Student1 student = new Student1("Agnes" , 25, "programming");
+        Student1 student = new Student1("Agnes" , 25, "G551234");
         student.introduce();
+
+        Teacher teacher = new Teacher("Javan", 35, "programming");
+        teacher.introduce();
 }
 
 }
