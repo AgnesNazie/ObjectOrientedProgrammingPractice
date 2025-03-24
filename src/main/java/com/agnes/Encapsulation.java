db2030b5e9a002d8915720eeb09d1d9fb3609619
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-24 19:15:41 +0100

    updated encapsulation

--- src/main/java/com/agnes/Encapsulation.java
+++ src/main/java/com/agnes/Encapsulation.java
@@ -3,15 +3,16 @@ package com.agnes;
 public class Encapsulation {
     private String name;
     private int grade;
+    private int age;
 
 
     void SetName(String name) {
         this.name = name;
     }
 
-    void SetGrade(int g) {
-        if (g >= 0 && g <= 100) {
-            grade = g;
+    void SetGrade(int grade) {
+        if (grade >= 0 && grade <= 100) {
+            this.grade = grade;
         } else {
             System.out.println("invalid Grade .");
         }
@@ -20,16 +21,34 @@ public class Encapsulation {
     String GetName() {
         return name;
     }
+    void SetAge(int age) {
+        if (age >= 5 &&  age <= 25 ){
+            this.age = age;
+        } else {
+            System.out.println("invalid age");
+        }
+    }
 
     int GetGrade() {
         return grade;
+    }
+    int GetAge() {
+        return age;
     }
 
     public static void main(String[] args) {
         Encapsulation e1 = new Encapsulation();
         e1.SetName("Agnes");
-        e1.SetGrade(85);
-        System.out.println("I'm " + e1.GetName() + " my grade is  " + e1.GetGrade());
+        e1.SetAge(4);
+        e1.SetGrade(0);
+        System.out.println("I'm " + e1.GetName() + " my grade is  " + e1.GetGrade() + " and i am " + e1.GetAge() + " years old");
+
+
+        Encapsulation e2 = new Encapsulation();
+        e2.SetName("fidelis");
+        e2.SetAge(30);
+        e2.SetGrade(90);
+        System.out.println("I'm " + e2.GetName() + " my grade is  " + e2.GetGrade() + " and i am " + e2.GetAge() + " years old");
 
 
     }
