bcf34dc288620265f8a46e661b0829d98b602e82
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-26 15:07:57 +0100

    created constructors for BankAccount

--- src/main/java/com/agnes/BankAccount.java
+++ src/main/java/com/agnes/BankAccount.java
@@ -1,7 +1,15 @@
 package com.agnes;
 
+import java.util.UUID;
+
 public class BankAccount {
     String accountNumber;
     double balance;
+
+    // create constructors
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = UUID.randomUUID().toString();
+         this.balance = balance;
+    }
 
 }
